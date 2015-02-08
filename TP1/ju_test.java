48a28a832acd5de3ae27cf6b58484295637584cd
edgd1er <edgd1er@hotmail.com>
2015-02-08 02:09:38 +0100

    Test Junit

---
+++ TP1/ju_test.java
@@ -0,0 +1,24 @@
+package Junit_ftp_server;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ju_test {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
