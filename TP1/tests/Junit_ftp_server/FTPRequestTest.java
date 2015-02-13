98624fba431e241f3d2289840ab73611233556e0
EmelineSlmn <salomon.emeline@gmail.com>
2015-02-13 10:29:11 +0100

    Ajout de la classe de test pour la classe FTPRequest.

---
+++ TP1/tests/Junit_ftp_server/FTPRequestTest.java
@@ -0,0 +1,14 @@
+package TP1.tests.Junit_ftp_server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FTPRequestTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
