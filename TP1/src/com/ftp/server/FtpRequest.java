9ec2b622c648f23d02989abf7b331588e6b91634
edgd1er <edgd1er@hotmail.com>
2015-02-17 21:29:36 +0100

    - version jar ok avec mdp.txt a coté - Attention de bien créer les repertoires utilisateurs avant le lancement...

--- TP1/src/com/ftp/server/FtpRequest.java
+++ TP1/src/com/ftp/server/FtpRequest.java
@@ -2,14 +2,11 @@ package com.ftp.server;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
